5e9dee416cb66b11b4541421f68e1b8b300031b4
Haseen Mathar <120903114+wizzseen@users.noreply.github.com>
2024-12-08 19:22:17 +0530

    Update checkpoint.py

--- yolov7/utils/checkpoint.py
+++ yolov7/utils/checkpoint.py
@@ -70,11 +70,11 @@ def load_state_dict(weights, model, map_location=None):
 
 def load_checkpoint(weights, map_location=None, inplace=True, fuse=True):
     """Load model from checkpoint file."""
-    LOGGER.info("Loading checkpoint from {}".format(weights))
+#    LOGGER.info("Loading checkpoint from {}".format(weights))
     ckpt = torch.load(weights, map_location=map_location)  # load
     model = ckpt["ema" if ckpt.get("ema") else "model"].float()
     if fuse:
-        LOGGER.info("\nFusing model...")
+ #       LOGGER.info("\nFusing model...")
         model = fuse_model(model).eval()
     else:
         model = model.eval()
